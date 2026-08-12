6c1edf8effd70883d9a1d42bb19f1ae4b5e6694b
che711 <che7111987@gmail.com>
2026-08-12 07:00:30 +0200

    added var lines to the main.go

--- main.go
+++ main.go
@@ -3,17 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("\nHello there! I'm a Go program!")
-	fmt.Print("I like the green color\n")
-	fmt.Println("Today is the 11 of August 2026\n")
+	// fmt.Println("\nHello there! I'm a Go program!")
+	// fmt.Print("I like the green color\n")
+	// fmt.Println("Today is the 11 of August 2026\n")
 	
 	
-	fmt.Print("Happy New 2027 Year!\n")
-	fmt.Println("     *   ")
-	fmt.Println("    ***   ")
-	fmt.Println("   *****   ")
-	fmt.Println("  *******   ")
-	fmt.Println(" *********   ")
-	fmt.Println("***********   ")
-	fmt.Println("    |||\n   ")
+	// fmt.Print("Happy New 2027 Year!\n")
+	// fmt.Println("     *   ")
+	// fmt.Println("    ***   ")
+	// fmt.Println("   *****   ")
+	// fmt.Println("  *******   ")
+	// fmt.Println(" *********   ")
+	// fmt.Println("***********   ")
+	// fmt.Println("    |||\n   ")
+
+	var a int
+	var b float32
+	var c float64
+	var d string
+	var e bool
+	fmt.Println(a, b , c, d, e)
+
+	
 }
