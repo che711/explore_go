37d21e66051840c9786595073b1f6f9f7fd9e56f
che711 <che7111987@gmail.com>
2026-08-25 08:04:28 +0200

    learned variables

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	// fmt.Println("\nHello there! I'm a Go program!")
 	// fmt.Print("I like the green color\n")
-	// fmt.Println("Today is the 11 of August 2026\n")
-	
-	
+	// fmt.Println("Today is the 11 of August 2026")
+	// fmt.Println("\n")
+
 	// fmt.Print("Happy New 2027 Year!\n")
 	// fmt.Println("     *   ")
 	// fmt.Println("    ***   ")
@@ -22,24 +22,30 @@ func main() {
 	// var c float64
 	// var d string
 	// var e bool
-	// fmt.Println(a, b , c, d, e)
+	// fmt.Println(a, b, c, d, e)
 
 	// count := 9
-	// price :=12.5
-	// total := float64 (count)*price
+	// price := 12.5
+	// total := float64(count) * price
 	// fmt.Println("Count: ", count)
 	// fmt.Println("Price: ", price)
 	// fmt.Println("Total: ", total)
 
-	const pi = 3.14
-	fmt.Println("Pi constant: ", pi)
-	
-	// checking seconds in the week
-	fmt.Println("Seconds in a week: ", 7*24*60*60)
+	// const pi = 3.14
+	// fmt.Println("Pi constant: ", pi)
 
-	fmt.Println("My friends kandy: ", 137/8)
-	fmt.Println("Only my kandy: ", 137%8)
-	
-	f_temp := 98.6
-	fmt.Println("Celsium temperature: ", ((f_temp -32)*5)/9)
+	// // checking seconds in the week
+	// fmt.Println("Seconds in a week: ", 7*24*60*60)
+
+	// fmt.Println("My friends kandy: ", 137/8)
+	// fmt.Println("Only my kandy: ", 137%8)
+
+	// f_temp := 98.6
+	// fmt.Println("Celsium temperature: ", ((f_temp-32)*5)/9)
+
+	// score := 10;
+	var score int;
+	fmt.Println("My score is: ", score);
+	score = 5;
+	fmt.Println("My score_2 is: ", score);
 }
